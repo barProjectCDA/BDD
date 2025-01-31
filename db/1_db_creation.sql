4c87c1036e879a6644a58cc948afa0f74fc9ef49
= <colonnact@outlook.com>
2025-01-31 10:52:47 +0100

    colonnaPrice&Status

--- db/1_db_creation.sql
+++ db/1_db_creation.sql
@@ -2,7 +2,7 @@ CREATE TABLE "category"(
    "id_category" SERIAL,
    "name" VARCHAR(50),
    "css_hexadecimal_color" VARCHAR(10),
-   "id_category_1" INTEGER NULL, -- Allowing null in case there's no parent category
+   "id_category_1" INTEGER NULL,
    PRIMARY KEY("id_category"),
    FOREIGN KEY("id_category_1") REFERENCES "category"("id_category") ON DELETE SET NULL
 );
@@ -43,6 +43,8 @@ CREATE TABLE "order"(
    "id_order" SERIAL,
    "id_client_table" INTEGER NOT NULL,
    "id_bar_user" INTEGER NOT NULL,
+   "price" NUMERIC (10,2),
+   "status" BOOLEAN,
    PRIMARY KEY("id_order"),
    FOREIGN KEY("id_client_table") REFERENCES "client_table"("id_client_table"),
    FOREIGN KEY("id_bar_user") REFERENCES "bar_user"("id_bar_user")
@@ -52,6 +54,8 @@ CREATE TABLE "order_product_extra"(
    "id_order_product_extra" SERIAL,
    "id_product" INTEGER NOT NULL,
    "id_order" INTEGER NOT NULL,
+   "price" NUMERIC (5,2),
+   "status" BOOLEAN,
    PRIMARY KEY("id_order_product_extra"),
    FOREIGN KEY("id_product") REFERENCES "product"("id_product"),
    FOREIGN KEY("id_order") REFERENCES "order"("id_order")
